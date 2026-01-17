e9324c6678392f6c3b82e1ddcfda51d5424751be
Arun Kumar <java.kumar.arun@gmail.com>
2026-01-17 11:56:50 +0530

    17 jan

---
+++ tutorial_11_string/string_1.py
@@ -0,0 +1,43 @@
+# male -> ramesh, suresh, mukesh
+# female -> sita, gita, rita
+
+# student -> marks, name, start_date
+# employee -> name, salary, joindate
+# singer -> name, album
+
+movie = "suPErmAN"
+
+print(f"Type: {type(movie)}") # str calss is category
+
+# print(f"Six Last char: {movie[-6]}")
+print(f"Fifth Last char: {movie[-5]}")
+print(f"Forth char: {movie[-4]}")
+print(f"Third Last char: {movie[-3]}")
+print(f"Second Last char: {movie[-2]}")
+print(f"Last char: {movie[-1]}")
+print(f"First char: {movie[0]}")
+print(f"Second char: {movie[1]}")
+print(f"Third char: {movie[2]}")
+print(f"Fourth char: {movie[3]}")
+print(f"Fifth char: {movie[4]}")
+# print(f"Sixth char: {movie[5]}")
+
+print(f"Total chars: {len(movie)}")
+print(f"Last char: {movie[len(movie)-1]}")
+
+# SUPER
+print(f"First 3 chars: {movie[0:3]}") # SUP
+print(f"First 3 chars: {movie[:3]}") # SUP
+
+print(f"First 4 chars: {movie[1:4]}") # UPE
+
+print(f"---: {movie[1:]}") # UPER
+
+
+print(f"Upper - {movie.upper()}")
+print(f"Lower - {movie.lower()}")
+# print(f"Cap - {movie.capitalize()}")
+
+
+# suPErmAN -> Superman
+# write code to print capitalized string
